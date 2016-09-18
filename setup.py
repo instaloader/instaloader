8408183b79b681f635ee354e7c4151fcc92e3198
Alexander Graf <mail@agraf.me>
2016-09-18 17:27:10 +0200

    minor, irrelevant setup.py fix

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet'
     ]
 )
