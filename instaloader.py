f1318470fbfa6df34409f4199bb8cdd10726bfaf
Lars Lindqvist <lars.lindqvist@yandex.ru>
2017-07-28 05:10:45 +0200

    s

--- instaloader.py
+++ instaloader.py
@@ -75,6 +75,10 @@ class InvalidArgumentException(NonfatalException):
     pass
 
 
+class BadResponseException(NonfatalException):
+    pass
+
+
 class BadCredentialsException(InstaloaderException):
     pass
 
@@ -371,7 +375,6 @@ class Instaloader:
 
     def save_caption(self, filename: str, date_epoch: float, caption: str) -> None:
         """Updates picture caption"""
-        # pylint:disable=too-many-branches
         filename += '.txt'
         pcaption = caption.replace('\n', ' ').strip()
         caption = caption.encode("UTF-8")
@@ -557,7 +560,6 @@ class Instaloader:
         :param download_comments: Update comments
         :return: True if something was downloaded, False otherwise, i.e. file was already there
         """
-        # pylint:disable=too-many-branches,too-many-locals,too-many-statements
         already_has_profilename = profile is not None or ('owner' in node and 'username' in node['owner'])
         needs_profilename = (format_string_contains_key(self.dirname_pattern, 'profile') or
                              format_string_contains_key(self.filename_pattern, 'profile'))
@@ -624,10 +626,7 @@ class Instaloader:
         self._log()
         return downloaded
 
-
     def download_stories(self,
-                         username: str,
-                         sessionfile: str,
                          download_videos: bool = True,
                          fast_update: bool = False) -> None:
         """
@@ -637,18 +636,21 @@ class Instaloader:
         :param download_videos: True, if videos should be downloaded
         """
 
-        self.load_session_from_file(username, sessionfile)
+        if self.username is None:
+            raise LoginRequiredException('Login required to download stories')
 
-        header = self.session.headers
-        header['User-Agent'] = 'Instagram 10.3.2 (iPhone7,2; iPhone OS 9_3_3; en_US; en-US; scale=2.00; 750x1334) AppleWebKit/420+'
+        tempsession = copy_session(self.session)
+        header = tempsession.headers
+        header['User-Agent'] = 'Instagram 10.3.2 (iPhone7,2; iPhone OS 9_3_3; en_US; en-US; scale=2.00; 750x1334) ' \
+                               'AppleWebKit/420+'
         del header['Host']
         del header['Origin']
         del header['X-Instagram-AJAX']
         del header['X-Requested-With']
 
-        self._log("Logged in as %s." % username)
+        self._sleep()
         url = 'https://i.instagram.com/api/v1/feed/reels_tray/'
-        resp = self.session.get(url)
+        resp = tempsession.get(url)
 
         if resp.status_code != 200:
             raise ConnectionException('Failed to fetch stories.')
@@ -656,7 +658,7 @@ class Instaloader:
         data = json.loads(resp.text)
 
         if not 'tray' in data:
-            raise InstaloaderException('Bad story reel JSON.')
+            raise BadResponseException('Bad story reel JSON.')
 
         totalcount = sum([len(us["items"]) if "items" in us else 0 for us in data["tray"]])
         count = 1
@@ -679,8 +681,8 @@ class Instaloader:
                     date /= 1000
                     date_stamp = datetime.datetime.fromtimestamp(date)
 
-                dirname = self.dirname_pattern.format(profile=name, target=name)
-                filename = dirname + '/' + self.filename_pattern.format(profile=name, target=name,
+                dirname = self.dirname_pattern.format(profile=name, target=':stories')
+                filename = dirname + '/' + self.filename_pattern.format(profile=name, target=':stories',
                                                                         date=date_stamp,
                                                                         shortcode=shortcode)
                 os.makedirs(dirname, exist_ok=True)
@@ -702,14 +704,13 @@ class Instaloader:
                                       date_epoch=date)
                     if "video_duration" in item and self.sleep:
                         time.sleep(item["video_duration"])
-                if len(item["story_locations"]) > 0:
+                if item["story_locations"]:
                     location = item["story_locations"][0]["location"]
                     if location:
                         self.save_location(filename, location, date)
                 self._log()
                 if fast_update and not downloaded:
                     break
-
 
     def download_feed_pics(self, max_count: int = None, fast_update: bool = False,
                            filter_func: Optional[Callable[[Dict[str, Dict[str, Any]]], bool]] = None,
@@ -734,7 +735,6 @@ class Instaloader:
         :param geotags: Download geotags
         :param download_comments: Update comments
         """
-        # pylint:disable=too-many-locals
         data = self.get_feed_json()
         count = 1
         while True:
@@ -863,7 +863,6 @@ class Instaloader:
                  profile_pic_only: bool = False, download_videos: bool = True, geotags: bool = False,
                  download_comments: bool = False, fast_update: bool = False) -> None:
         """Download one profile"""
-        # pylint:disable=too-many-branches,too-many-locals
         # Get profile main page json
         data = self.get_json(name)
         # check if profile does exist or name has changed since last download
@@ -929,7 +928,6 @@ class Instaloader:
                           download_comments: bool = False,
                           fast_update: bool = False) -> None:
         """Download set of profiles and handle sessions"""
-        # pylint:disable=too-many-branches,too-many-locals,too-many-statements
         # Login, if desired
         if username is not None:
             try:
