instaloader/instaloader

commit 0ad50c1526a1270684f352df9bfb4dc5f3e01cb9

Fix error on downloading own private profile
Closes #27.

instaloader.py
@@ -910,7 +910,8 @@ class Instaloader:
         if data["entry_data"]["ProfilePage"][0]["user"]["is_private"]:
             if data["config"]["viewer"] is None:
                 raise LoginRequiredException("profile %s requires login" % name)
-            if not data["entry_data"]["ProfilePage"][0]["user"]["followed_by_viewer"]:
+            if not data["entry_data"]["ProfilePage"][0]["user"]["followed_by_viewer"] and \
+                    self.username != data["entry_data"]["ProfilePage"][0]["user"]["username"]:
                 raise PrivateProfileNotFollowedException("Profile %s: private but not followed." % name)
         else:
             if data["config"]["viewer"] is not None and not (download_stories or download_stories_only):
@@ -1155,7 +1156,7 @@ def main():
         loader = Instaloader(sleep=not args.no_sleep, quiet=args.quiet, shorter_output=args.shorter_output,
                              user_agent=args.user_agent,
                              dirname_pattern=args.dirname_pattern, filename_pattern=args.filename_pattern)
-        loader.download_profiles(args.profile, args.login, args.password, args.sessionfile,
+        loader.download_profiles(args.profile, args.login.lower(), args.password, args.sessionfile,
                                  int(args.count) if args.count is not None else None,
                                  args.profile_pic_only, not args.skip_videos, args.geotags, args.comments,
                                  args.fast_update, args.stories, args.stories_only)

commit 46ac119a10a3fcae62a6f489ceeffdc70268feae

Fix AttributeError when not logging in
Mentioned in #26. Bug was introduced in 0ad50c1.

instaloader.py
@@ -1157,7 +1157,8 @@ def main():
         loader = Instaloader(sleep=not args.no_sleep, quiet=args.quiet, shorter_output=args.shorter_output,
                              user_agent=args.user_agent,
                              dirname_pattern=args.dirname_pattern, filename_pattern=args.filename_pattern)
-        loader.download_profiles(args.profile, args.login.lower(), args.password, args.sessionfile,
+        loader.download_profiles(args.profile, args.login.lower() if args.login is not None else None, args.password,
+                                 args.sessionfile,
                                  int(args.count) if args.count is not None else None,
                                  args.profile_pic_only, not args.skip_videos, args.geotags, args.comments,
                                  args.fast_update, args.stories, args.stories_only)