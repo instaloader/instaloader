5d83a4ccf660a229151baac7df859551fd02b518
Alexander Graf <mail@agraf.me>
2017-08-06 10:49:36 +0200

    Release of version 2.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='instaloader',
-    version='2.2.1',
+    version='2.2.2',
     py_modules=['instaloader'],
     url='https://github.com/Thammus/instaloader',
     license='MIT',
