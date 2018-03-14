68d8f65cb9dcc3d9ead8a409a5ee3c9877d93b97
Alexander Graf <mail@agraf.me>
2018-03-14 22:14:24 +0100

    Add logo in documentation sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -159,7 +159,7 @@ html_theme_options = {
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-# html_logo = None
+html_logo = "logo.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
