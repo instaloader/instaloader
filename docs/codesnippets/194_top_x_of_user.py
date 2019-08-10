75a729781d065b35ad82c2e3048f616ab723054f
Alexander Graf <mail@agraf.me>
2019-08-10 16:25:22 +0200

    194_top_x_of_user example: Fix sort order

--- docs/codesnippets/194_top_x_of_user.py
+++ docs/codesnippets/194_top_x_of_user.py
@@ -9,7 +9,9 @@ X_percentage = 10    # percentage of posts that should be downloaded
 L = Instaloader()
 
 profile = Profile.from_username(L.context, PROFILE)
-posts_sorted_by_likes = sorted(profile.get_posts(), key = lambda p: p.likes + p.comments)
+posts_sorted_by_likes = sorted(profile.get_posts(),
+                               key=lambda p: p.likes + p.comments,
+                               reverse=True)
 
 for post in islice(posts_sorted_by_likes, ceil(profile.mediacount * X_percentage / 100)):
     L.download_post(post, PROFILE)
