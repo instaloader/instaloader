7068a75f99f173f18b0837c3bc4d93e5da77e68c
Mehmet Salih Bindak <mehmetsalihbindak@gmail.com>
2019-08-23 16:25:32 +0300

    some fix for bat file

--- instaloader.bat
+++ instaloader.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 cls
-echo Sayfanin ismini yaz
 set /p id="Enter Instagram Username: "
 python instaloader.py --no-metadata-json --no-video-thumbnails --no-captions --no-compress-json profile %id%
 move %id% C:\Users\%username%\Desktop
+echo Download completed!
 PAUSE
 
 
