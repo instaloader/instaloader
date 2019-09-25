b862e4ef2a708a55cda338127d0ecf0ea7e5f8cb
Leonard Bogdonoff <lkbgift@gmail.com>
2019-09-24 22:39:06 -0400

    Update 113_only_one_per_user.py

--- docs/codesnippets/113_only_one_per_user.py
+++ docs/codesnippets/113_only_one_per_user.py
@@ -8,7 +8,7 @@ users = set()
 
 for post in posts:
     if not post.owner_profile in users:
-        L.download_post(post, '#')
+        L.download_post(post, '#urbanphotography')
         users.add(post.owner_profile)
     else:
         print("{} from {} skipped.".format(post, post.owner_profile))
