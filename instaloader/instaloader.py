f910705c51a4e6eb2d31a5a097e13b0cd66e1062
Alexander Graf <17130992+aandergr@users.noreply.github.com>
2020-03-07 11:14:07 +0100

    docs: Fix formatting of request_timeout param

--- instaloader/instaloader.py
+++ instaloader/instaloader.py
@@ -142,7 +142,7 @@ class Instaloader:
     :param storyitem_metadata_txt_pattern: :option:`--storyitem-metadata-txt`, default is empty (=none)
     :param max_connection_attempts: :option:`--max-connection-attempts`
     :param commit_mode: :option:`--commit-mode`
-    :param request_timeout: "option:`--request-timeout`, set per-request timeout (seconds)
+    :param request_timeout: :option:`--request-timeout`, set per-request timeout (seconds)
 
     .. attribute:: context
 
