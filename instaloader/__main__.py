8f9723cf62625d0a26b33e3d8de13287c1ba1d76
Alexander Graf <17130992+aandergr@users.noreply.github.com>
2020-06-08 18:40:13 +0200

    Fix "Use --login ..." warning at startup

--- instaloader/__main__.py
+++ instaloader/__main__.py
@@ -197,7 +197,7 @@ def _main(instaloader: Instaloader, targetlist: List[str],
         if len(profiles) > 1:
             instaloader.context.log("Downloading {} profiles: {}".format(len(profiles),
                                                                          ' '.join([p.username for p in profiles])))
-        if ((profiles and download_profile_pic) or download_posts) and not instaloader.context.is_logged_in:
+        if profiles and (download_profile_pic or download_posts) and not instaloader.context.is_logged_in:
             instaloader.context.error("Warning: Use --login to download higher-quality versions of pictures.")
         instaloader.download_profiles(profiles,
                                       download_profile_pic, download_posts, download_tagged, download_igtv,
