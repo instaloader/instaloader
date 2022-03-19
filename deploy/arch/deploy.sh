3cbee1b3188a484e8b9613ee8c73c9a659f3b629
Alexander Graf <17130992+aandergr@users.noreply.github.com>
2022-03-19 16:27:11 +0100

    Fully Migrate to GitHub Actions

--- deploy/arch/deploy.sh
+++ deploy/arch/deploy.sh
@@ -3,15 +3,16 @@
 VERSION=${1:1}
 
 # do not deploy pre-releases
-echo $VERSION | grep -qe "[abc]" && exit 0
+#echo $VERSION | grep -qe "[abc]" && exit 0
 
 cd $(dirname $0)
 
 # decrypt and add ssh key
-openssl aes-256-cbc -K $encrypted_354637631c28_key -iv $encrypted_354637631c28_iv -in id_rsa_AUR.enc -out /tmp/AUR_openssh -d
+openssl aes-256-cbc -K $AUR_KEY -iv $AUR_IV -in id_rsa_AUR.enc -out /tmp/AUR_openssh -d
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/AUR_openssh
 ssh-add /tmp/AUR_openssh
+ssh-keyscan -t rsa aur.archlinux.org >> ~/.ssh/known_hosts
 
 # clone and modify AUR repo
 git clone --depth 1 ssh://aur@aur.archlinux.org/instaloader.git
--- deploy/windows/create_exe.py
+++ deploy/windows/create_exe.py
@@ -44,7 +44,7 @@ with open('__main__.py', 'w+') as f:
     f.writelines(lines)
 
 # install dependencies and invoke PyInstaller
-commands = ["pip install pipenv==2020.11.15",
+commands = ["pip install pipenv==2022.1.8",
             f"pipenv --python {sys.version_info.major}.{sys.version_info.minor} sync --dev",
             "pipenv run pyinstaller --log-level=DEBUG instaloader.spec"]
 
