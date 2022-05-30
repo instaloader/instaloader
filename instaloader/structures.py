5a240640126e556ba70c8132ed5e5be5b9ef4ac5
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-05-30 21:29:34 +0200

    Fix incorrect error messages when certain requests fail (#1536)
    - handle case where `Profile.from_id` works, but `Profile.from_username` doesn't
    
    - Handle case where `QueryReturnedNotFoundException` lies and `similar_profiles` contains the exact username
    
    - Still raises `ProfileNotExistsException` for compatibility.

--- instaloader/structures.py
+++ instaloader/structures.py
@@ -771,6 +771,9 @@ class Profile:
             top_search_results = TopSearchResults(self._context, self.username)
             similar_profiles = [profile.username for profile in top_search_results.get_profiles()]
             if similar_profiles:
+                if self.username in similar_profiles:
+                    raise ProfileNotExistsException(
+                        f"Profile {self.username} seems to exist, but could not be loaded.") from err
                 raise ProfileNotExistsException('Profile {} does not exist.\nThe most similar profile{}: {}.'
                                                 .format(self.username,
                                                         's are' if len(similar_profiles) > 1 else ' is',
