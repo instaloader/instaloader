062c0b2a56015422b93550399b1c73cfab21a16e
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2023-06-18 18:02:43 -0300

    Configurando teste para _get_default_session_filename Windows

--- testes-unitarios/testes-unitarios.py
+++ testes-unitarios/testes-unitarios.py
@@ -4,7 +4,6 @@ import unittest
 from unittest.mock import patch
 
 from instaloader import instaloader
-print(instaloader.get_default_session_filename('kaell_andrade'))
 USUARIO_TESTE = 'kaell_andrade'
 WINDOS = 'Windows'
 UNIX = 'Unix'
@@ -12,25 +11,33 @@ UNIX = 'Unix'
 class TestesUnitarios(unittest.TestCase):
 
     @patch('platform.system')
-    def test_windows_localappdata(self, mock_system):
+    def test_windows_localappdata_get_config_dir(self, mock_system):
         mock_system.return_value = WINDOS
-        os.environ["LOCALAPPDATA"] = "C:/Users/{}/AppData/Local".format(USUARIO_TESTE)
-        expected_dir = os.path.normpath("C:/Users/{}/AppData/Local/Instaloader".format(USUARIO_TESTE))
+        os.environ["LOCALAPPDATA"] = "C:\\Users\\{}\\AppData\\Local".format(USUARIO_TESTE)
+        expected_dir = os.path.normpath("C:\\Users\\{}\\AppData\Local/Instaloader".format(USUARIO_TESTE))
         self.assertEqual(instaloader._get_config_dir(), expected_dir)
     
     @patch('platform.system')
-    def test_windows_fallback(self, mock_system):
+    def test_windows_fallback_get_config_dir(self, mock_system):
         mock_system.return_value = WINDOS
         getpass.getuser = lambda: USUARIO_TESTE
         expected_dir = os.path.normpath("/tmp/.instaloader-{}".format(USUARIO_TESTE))
         self.assertEqual(instaloader._get_config_dir(), expected_dir)
     
     @patch('platform.system')
-    def test_unix(self, mock_system):
+    def test_unix_get_config_dir(self, mock_system):
         mock_system.return_value = UNIX
         os.environ["XDG_CONFIG_HOME"] = "/home/{}/.config".format(USUARIO_TESTE)
         expected_dir = "/home/{}/.config/instaloader".format(USUARIO_TESTE)
         self.assertEqual(instaloader._get_config_dir(), expected_dir)
+    
+    @patch('platform.system')
+    def test_windows_get_default_session_filename(self, mock_system):
+        mock_system.return_value = "Windows"
+        os.environ["LOCALAPPDATA"] = "C:\\Users\\testuser\\AppData\\Local"
+        expected_filename = "C:\\Users\\testuser\\AppData\\Local/Instaloader/session-{}".format(USUARIO_TESTE)
+        self.assertEqual(instaloader.get_default_session_filename(USUARIO_TESTE), expected_filename)
+
 
 if __name__ == '__main__':
     unittest.main()
