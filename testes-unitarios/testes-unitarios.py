cf3134239bee5abf29cd7b917d2a3475f68676e3
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2023-06-18 15:56:22 -0300

    Criando ambiente de testes unitários.

---
+++ testes-unitarios/testes-unitarios.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestesUnitarios(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
