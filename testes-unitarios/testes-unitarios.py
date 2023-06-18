c05ab8dc3d61e0c7fff58a39d96b821ccded4e60
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2023-06-18 17:35:14 -0300

    Criando teste unitários para _get_configdir() com mock.

--- testes-unitarios/testes-unitarios.py
+++ testes-unitarios/testes-unitarios.py
@@ -1,9 +1,22 @@
+import os
+import getpass
 import unittest
+from unittest.mock import patch
+
+from instaloader import instaloader
+print(instaloader.get_default_session_filename('kaell_andrade'))
+USUARIO_TESTE = 'kaell_andrade'
+WINDOS = 'Windows'
+UNIX = 'Unix'
 
 class TestesUnitarios(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    @patch('platform.system')
+    def test_windows_localappdata(self, mock_system):
+        mock_system.return_value = WINDOS
+        os.environ["LOCALAPPDATA"] = "C:/Users/{}/AppData/Local".format(USUARIO_TESTE)
+        expected_dir = os.path.normpath("C:/Users/{}/AppData/Local/Instaloader".format(USUARIO_TESTE))
+        self.assertEqual(instaloader._get_config_dir(), expected_dir)
 
 if __name__ == '__main__':
     unittest.main()
