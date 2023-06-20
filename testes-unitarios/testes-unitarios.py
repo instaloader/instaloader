d585381428b0ce680c87adbd8d48991791f122d5
gabrielbtera <gabriel.jesus@dcomp.ufs.br>
2023-06-20 00:23:47 -0300

    tests: with datelocal

--- testes-unitarios/testes-unitarios.py
+++ testes-unitarios/testes-unitarios.py
@@ -14,40 +14,49 @@ from instaloader.structures import Post, Profile
 
 import time
 
-timestamp = int(time.time())
-print("Timestamp atual:", timestamp, time.time())
+
 
 import datetime
 
-dt = datetime.datetime.fromtimestamp(timestamp)
-
-print("Data e hora capturadas:", dt)
 
 
 
-
-class MockPost:
+class TestsPost:
     def __init__(self) -> None:
         self.instaLoader = Instaloader()
         self.user = 'ufsoficial'
-        self.__initializeInstaloader()
-        pass
+        self.timestamp = 1687140049
+        
+        # self.__initializeInstaloader()
 
     def __initializeInstaloader(self) -> None:
         # self.instaLoader.login(self.user, '')
         post = Post.from_shortcode(self.instaLoader.context, 'Cs894TXOetY')
         profile = Profile.from_username(self.instaLoader.context, self.user )
 
-        post2 = Post(self.instaLoader.context, {'shortcode': 'Cs894TXOetY', 'id': '1234', 'title': 'post da ufs', 'date': self.date})
+        post2 = Post(self.instaLoader.context, self.mockpost)
         print(profile, post2)
         print('aqui', post.shortcode)
         print('title', post2.title)
-        print('datelocal', type(post.date))
+        print()
+        print('datelocal', post._node.keys())
+    
+    def util_datetime(self):
+        return datetime.datetime.fromtimestamp(self.timestamp)
     @property
-    def date(self):
-        timestamp = 1687140049
-        
-        return datetime.datetime.fromtimestamp(timestamp)
+    def mockpost(self):
+        return  {   
+                'owner': 'gabriel', 
+                'shortcode': 'Cs894TXOetY', 
+                'id': '1234', 
+                'title': 'post da ufs', 
+                'date': self.timestamp, 
+                'taken_at_timestamp': self.timestamp
+                }
+
+    
+
+    
 
 
         
@@ -56,10 +65,41 @@ class MockPost:
 
 class TestesUnitarios(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    data_tests = TestsPost()
+    mockpost = data_tests.mockpost
+    timestamp = data_tests.timestamp
+
+
+
+    def test_local_date_with_date_key(self):
+        from_timestamp = self.data_tests.util_datetime().astimezone()
+
+        post = Post(self.data_tests.instaLoader.context, self.mockpost)
+
+        self.assertEqual(post.date_local, from_timestamp)
+    
+    def test_local_date_without_date_key(self):
+        copy_date = self.mockpost.copy()
+        del copy_date['date']
+
+        from_timestamp = self.data_tests.util_datetime().astimezone()
+
+        post = Post(self.data_tests.instaLoader.context, self.mockpost)
+
+        self.assertEqual(post.date_local, from_timestamp)
+
+
+
+    
+
+
+    
+
+        
+        
+   
 
 if __name__ == '__main__':
-    mock = MockPost()
+    mock = TestsPost()
 
     unittest.main()
