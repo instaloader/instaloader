f6ce0a3f5093e48a4d5aed41abeee88f99685361
gabrielbtera <gabriel.jesus@dcomp.ufs.br>
2023-07-12 00:15:57 -0300

    refactor: set mocks in tests

--- testes-unitarios/testes-unitarios.py
+++ testes-unitarios/testes-unitarios.py
@@ -4,6 +4,9 @@ import datetime
 
 from unittest.mock import patch
 
+from unittest import mock
+from datetime import datetime, timezone
+
 from instaloader import instaloader
 USUARIO_TESTE = 'kaell_andrade'
 WINDOS = 'Windows'
@@ -75,7 +78,9 @@ class TestesUnitarios(unittest.TestCase):
     data_tests = MockTestsPost()
     mockpost = data_tests.mockpost
     timestamp = data_tests.timestamp
-    context = data_tests.instaLoader.context
+    # context = data_tests.instaLoader.context
+
+    context = mock.Mock(spec=Instaloader)
 
     @patch('platform.system')
     def test_windows_localappdata_get_config_dir(self, mock_system):
@@ -157,74 +162,124 @@ class TestesUnitarios(unittest.TestCase):
 
     # /////// Tests dinamics properties
 
+    # def test_local_date_with_date_key(self):
+    #     print('test_local_date_with_date_key')
+    #     from_timestamp = self.data_tests.util_datetime().astimezone()
+
+    #     post = Post(self.context, self.mockpost)
+
+    #     self.assertEqual(post.date_local, from_timestamp)
+    
+    # def test_local_date_without_date_key(self):
+    #     print('test_local_date_without_date_key')
+    #     copy_date = self.mockpost.copy()
+    #     del copy_date['date']
+
+    #     from_timestamp = self.data_tests.util_datetime().astimezone()
+
+    #     post = Post(self.context, self.mockpost)
+
+    #     self.assertEqual(post.date_local, from_timestamp)
+    
+    # def test_with_title_post(self):
+    #     print('test_with_title_post')
+    #     title = 'post da ufs'
+
+    #     post = Post(self.context,  self.mockpost)
+        
+    #     self.assertEqual(post.title, title)
+    
+
+    # def test_without_title_post(self):
+    #     print('test_without_title_post')
+    #     title = None
+    #     copymock = self.mockpost.copy()
+
+    #     del copymock['title']
+    #     post = Post(self.context, copymock)
+    #     post._full_metadata_dict = {'teste': 'title'}
+
+    #     self.assertEqual( post.title, title)
+
+
+    # def test_media_id(self):
+    #     print('test_media_id')
+    #     mediaid = 1234
+    #     post = Post(self.context,  self.mockpost)
+
+    #     self.assertEqual(mediaid, post.mediaid)
+    
+    
+
+    # def test_shortcode_with_shortcode_key(self):
+    #     print('test_shortcode_with_shortcode_key')
+    #     shortcode = 'shortcode123'
+    #     post = Post(self.context, self.mockpost)
+
+    #     self.assertEqual(post.shortcode, shortcode)
+    
+    # def test_shortcode_without_shortcode_key(self):
+    #     print('test_shortcode_without_shortcode_key')
+    #     shortcode = 'shortcode123'
+
+    #     copymock = self.mockpost.copy()
+    #     del copymock['shortcode']
+
+    #     post = Post(self.context, copymock)
+
+    #     self.assertEqual(post.shortcode, shortcode)
+    
+
     def test_local_date_with_date_key(self):
         print('test_local_date_with_date_key')
-        from_timestamp = self.data_tests.util_datetime().astimezone()
+        with mock.patch.object(self.data_tests, 'util_datetime', return_value=datetime.now(timezone.utc)):
+            post = Post(self.context, self.data_tests.mockpost)
+            expected_date = datetime.fromtimestamp(self.data_tests.timestamp, timezone.utc)
+            self.assertEqual(post.date_local, expected_date)
 
-        post = Post(self.context, self.mockpost)
-
-        self.assertEqual(post.date_local, from_timestamp)
-    
     def test_local_date_without_date_key(self):
         print('test_local_date_without_date_key')
-        copy_date = self.mockpost.copy()
+        copy_date = self.data_tests.mockpost.copy()
         del copy_date['date']
+        with mock.patch.object(self.data_tests, 'util_datetime', return_value=datetime.now(timezone.utc)):
+            post = Post(self.context, self.data_tests.mockpost)
+            expected_date = datetime.fromtimestamp(self.data_tests.timestamp, timezone.utc)
+            self.assertEqual(post.date_local, expected_date)
 
-        from_timestamp = self.data_tests.util_datetime().astimezone()
-
-        post = Post(self.context, self.mockpost)
-
-        self.assertEqual(post.date_local, from_timestamp)
-    
     def test_with_title_post(self):
         print('test_with_title_post')
         title = 'post da ufs'
-
-        post = Post(self.context,  self.mockpost)
-        
+        post = Post(self.context, self.data_tests.mockpost)
         self.assertEqual(post.title, title)
-    
 
     def test_without_title_post(self):
         print('test_without_title_post')
         title = None
-        copymock = self.mockpost.copy()
-
+        copymock = self.data_tests.mockpost.copy()
         del copymock['title']
         post = Post(self.context, copymock)
         post._full_metadata_dict = {'teste': 'title'}
-
-        self.assertEqual( post.title, title)
-
+        self.assertEqual(post.title, title)
 
     def test_media_id(self):
         print('test_media_id')
         mediaid = 1234
-        post = Post(self.context,  self.mockpost)
-
+        post = Post(self.context, self.data_tests.mockpost)
         self.assertEqual(mediaid, post.mediaid)
-    
-    
 
     def test_shortcode_with_shortcode_key(self):
         print('test_shortcode_with_shortcode_key')
         shortcode = 'shortcode123'
-        post = Post(self.context, self.mockpost)
-
+        post = Post(self.context, self.data_tests.mockpost)
         self.assertEqual(post.shortcode, shortcode)
-    
+
     def test_shortcode_without_shortcode_key(self):
         print('test_shortcode_without_shortcode_key')
         shortcode = 'shortcode123'
-
-        copymock = self.mockpost.copy()
+        copymock = self.data_tests.mockpost.copy()
         del copymock['shortcode']
-
         post = Post(self.context, copymock)
-
         self.assertEqual(post.shortcode, shortcode)
-
-   
 
 if __name__ == '__main__':
 
